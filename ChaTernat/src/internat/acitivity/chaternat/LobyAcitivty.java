678324e41f329e5220687e0764cf7fda4bc5e075
Ben <Ben@SKYNET>
2014-02-03 14:06:55 +0100

    Fix question 193, 501, 910, 228, 1209, 600, 1586, 209

--- ChaTernat/src/internat/acitivity/chaternat/LobyAcitivty.java
+++ ChaTernat/src/internat/acitivity/chaternat/LobyAcitivty.java
@@ -15,7 +15,7 @@ public class LobyAcitivty extends Activity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loby);        
+        setContentView(R.layout.activity_loby);       
         
     }
 
